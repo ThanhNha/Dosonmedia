ce05dd9117217da55d7385e8c9151c363c068db7
ThanhNha <72262090+ThanhNha@users.noreply.github.com>
2022-05-30 13:17:45 +0700

    done

--- src/wp-content/themes/doson/site-structure/footer/script.js
+++ src/wp-content/themes/doson/site-structure/footer/script.js
@@ -1,28 +1,16 @@
 function footer() {
-  var backtop = gsap.to("#backtop", {
-    opacity: "1",
-    duration: 0.1,
-    ease: "power2.in",
-    paused: true,
+  var backtop = $("#backtop");
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      backtop.addClass("show");
+    } else {
+      backtop.removeClass("show");
+    }
   });
-  //fixed backtop
-  ScrollTrigger.create({
-    trigger: "#backtop",
-    start: "200px top",
-    end: 99999,
-    onUpdate: ({ progress, direction, isActive }) => {
-      if (direction == -1) {
-        backtop.reverse();
-      }
-      if (direction == 1) {
-        backtop.play();
-      } else if (direction == 1 && isActive == true) {
-        backtop.play();
-      }
-    },
-  });
-  $(".backtop").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
+
+  backtop.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
   });
 }
